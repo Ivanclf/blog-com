1803cc5c218ffad447cbe97734ff51bdb77a0e79
Ivanclf <23009201447@stu.xidian.edu.cn>
2025-07-13 21:22:18 +0800

    try to make it 1st

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
+      }
+    }
+  }
 })
